ca6395bac6173be33f5f9064f545904abd824e6f
Hemant.D <hemant.d@simplify3x.com>
2024-05-01 08:50:50 +0530

    Hide Navbar on Scroll

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,31 @@
+import { useEffect, useRef } from "react"
+import "../styles/Navbar.css";
+
 export const Navbar = () => {
+
+    //refs
+    const nav = useRef(null);
+
+    //Hide on Scroll
+    let scrollY = 0;
+    useEffect(()=>{
+        const hideOnScroll = () => {
+            if(window.scrollY > scrollY) {
+                nav.current.classList.add("hide");
+            }
+            else {
+                nav.current.classList.remove("hide");
+            }
+            scrollY = window.scrollY;
+        }
+
+        window.addEventListener("scroll", hideOnScroll);
+
+        return () => window.removeEventListener("scroll", hideOnScroll);
+    },[])
+
     return (
-        <nav className="fixed top-0 left-0 w-screen z-50">
+        <nav ref={nav} className="fixed top-0 left-0 w-screen z-50">
             <div className="navbar-container flex gap-5 items-center justify-between p-5 text-white">
                 <div className="left">
                     <span>About</span>
