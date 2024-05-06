308fb54e0cc188deacc9c3f73dbe84f3158aa24d
Hemant.D <hemant.d@simplify3x.com>
2024-05-06 11:20:12 +0530

    Create About Page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
             <footer className="px-5 py-2">
                 <div className="footer-container w-full flex items-center justify-between">
                     <div className="credits">
-                        <span className="text-white text-sm">&copy; SENTINEL by <a href="https://github.com/HemantDutta" className="underline underline-offset-2" rel="noreferrer" target="_blank">Hemant Dutta</a></span>
+                        <a href="https://github.com/HemantDutta/Sentinel" rel="noreferrer" target="_blank" className="text-white text-sm">&copy; SENTINEL by <a href="https://github.com/HemantDutta" className="underline underline-offset-2" rel="noreferrer" target="_blank">Hemant Dutta</a></a>
                     </div>
                     <div className="sitemap flex items-center gap-3 flex-wrap text-sm">
                         <Link to="/" className="text-white transition hover:underline underline-offset-2">Home</Link>
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -9,13 +9,13 @@ export const About = () => {
             <Navbar />
             {/* Navbar End */}
             <div className="about-page pt-24">
-                <div className="about-flex flex flex-col p-5">
+                <div className="about-flex flex flex-col p-5 mb-20">
                     <div className="panel-1 h-[500px] flex items-center justify-center text-center">
                         <span className="text-8xl bit-font text-gradient drop-shadow-lg">How Much Can They See?</span>
                     </div>
                     <div className="panel-2 pt-48 flex items-center justify-between flex-wrap">
                         <div className="project-info text-white w-[100%] md:w-[50%] text-center md:text-left flex flex-col gap-2">
-                            <span className="text-gradient-2">Why did I make this project?</span>
+                            <span className="text-gradient-2 font-bold">Why did I make this project?</span>
                             <div className="info-text">
                                 <span className="first text-3xl bit-font">One</span> <span>day while browsing the Internet, this question popped up in my head! How much information can websites legally/ethically gain about us from our Browser? And that is how <span className="bit-font text-2xl text-gradient-2">Sentinel</span> was born. Sentinel's only purpose is to show you how much information your browser is revealing to the websites you visit!</span>
                             </div>
@@ -27,12 +27,12 @@ export const About = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="panel-3 pt-48 flex items-center justify-between flex-wrap">
-                        <div className="title w[100%] md:w-[50%]">
+                    <div className="panel-3 pt-48 flex items-stretch justify-between flex-wrap">
+                        <div className="title grid place-items-center w-[100%] md:w-[50%]">
                             <span className="text-6xl bit-font text-gradient drop-shadow-lg">So What's going on here?</span>
                         </div>
                         <div className="logic-info w[100%] md:w-[50%] flex flex-col gap-2">
-                            <span className="text-gradient-2">How does it work?</span>
+                            <span className="text-gradient-2 font-bold">How does it work?</span>
                             <span className="text-white">Are you being hacked? Is someone looking into your laptop screen through your window? No, nothing of that sort is happening (Well, I am not sure about the window thing, someone might be there, I don't know).</span>
                             <span className="text-white">All of this information is openly available to any website using vanilla JavaScript, Web APIs such as navigator, Global Objects like "document" and "window" and a few other things.</span>
                             <span className="text-white">So imagine, if a regular website can ethically get all this information about you, what information can a person with no ethics and morals find out about you through the Internet.</span>
@@ -42,7 +42,7 @@ export const About = () => {
                 </div>
             </div>
             {/* Footer */}
-            {/* <Footer/> */}
+            <Footer/>
             {/* Footer End */}
         </>
     )
